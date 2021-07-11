74462aa19d17a92e5f05a4052a3c9661487b24ae
leo <leo@163.com>
2021-07-11 23:23:02 +0800

    master test commit

--- git_demo.java
+++ git_demo.java
@@ -2,5 +2,6 @@ public class git_demo {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello java");
+        System.out.println("master test");
     }
 }
